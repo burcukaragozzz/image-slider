564fcdb679a1eea0e7c4313d235c68ef0618ed93
Burcu Karagoz <burcu.karagoz@metglobal.com>
2019-10-11 11:14:58 +0300

    updated

--- src/App.js
+++ src/App.js
@@ -18,7 +18,6 @@ class App extends Component {
     })
       .then(res => res.json())
       .then(response => {
-        console.log(response[0]);
         this.setState({images: response})
       })
       .catch(error => console.error('Error:', error));
@@ -28,15 +27,11 @@ class App extends Component {
   goToPrevSlide = (event) => {
     var code = (event.which) ? event.which : event.keyCode;
 
-    if((this.state.currentIndex === 0)||kc == 37) {
-      return;
-    }
-
     this.setState({
       currentIndex: this.state.currentIndex - 1
     });
   }
-  
+
   goToNextSlide = () => {
 
     if(this.state.currentIndex === this.state.images.length - 1) {
@@ -48,7 +43,7 @@ class App extends Component {
     });
   }
 
-  
+
   render() {
     const { images, currentIndex } = this.state;
     const sliderItems = images.map(function(item, index) {
@@ -63,20 +58,16 @@ class App extends Component {
 
     return(
         <div className="content">
-          <div className="slider">
           <i className={`fa fa-arrow-left left-icon ${currentIndex===0 ? "deactive" : ""}`} onClick={this.goToPrevSlide}></i>
+          <div className="slider">
             <div className="image-wrapper">
               {sliderItems}
-            </div> 
-            <i className={`fa fa-arrow-right right-icon ${images.length - 1 === currentIndex ? "deactive" : ""}`} onClick={this.goToNextSlide}></i>
+            </div>
           </div>
-
+          <i className={`fa fa-arrow-right right-icon ${images.length - 1 === currentIndex ? "deactive" : ""}`} onClick={this.goToNextSlide}></i>
         </div>
     );
   }
 }
 
 export default App;
-
-
-
